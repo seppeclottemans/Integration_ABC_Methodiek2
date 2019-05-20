c3618f55cc3bea4081dc8c2a8d16fe656e5c6fa4
Seppe Clottemans <>
2019-05-20 09:47:05 +0200

    nav

--- js/dragEnDrop.js
+++ js/dragEnDrop.js
@@ -1,13 +1,43 @@
 $(function () {
-  $(function snapDrag() {
-    $( "#kaart1" ).draggable({ snap: ".snapTarget",});
-    $( "#kaart2" ).draggable({ snap: ".snapTarget" });
-    $( "#kaart3" ).draggable({ snap: ".snapTarget" });
-    $( "#kaart4" ).draggable({ snap: ".snapTarget" });
-    $( "#kaart5" ).draggable({ snap: ".snapTarget" });
-    $( "#kaart6" ).draggable({ snap: ".snapTarget" });
-  });
-    $(function Sort() {
-    $( "#kaartjes" ).sortable({revert: true});
-    })
+    $(function snapDrag() {
+        $("#snapTarget").sortable({
+            revert: true
+        });
+        $("#kaart1").draggable({
+            snap: ".snapTarget",
+            connectToSortable: "#snapTarget",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("#kaart2").draggable({
+            snap: ".snapTarget",
+            connectToSortable: "#snapTarget",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("#kaart3").draggable({
+            snap: ".snapTarget",
+            connectToSortable: "#snapTarget",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("#kaart4").draggable({
+            snap: ".snapTarget",
+            connectToSortable: "#snapTarget",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("#kaart5").draggable({
+            snap: ".snapTarget",
+            connectToSortable: "#snapTarget",
+            helper: "clone",
+            revert: "invalid"
+        });
+        $("#kaart6").draggable({
+            snap: ".snapTarget",
+            connectToSortable: "#snapTarget",
+            helper: "clone",
+            revert: "invalid"
+        });
+    });
 })
