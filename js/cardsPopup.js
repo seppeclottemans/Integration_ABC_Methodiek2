0e6be4a3d3464fcfd0e4e13095384c7cabcdc71c
Jan-Baptist De Bondt <jan-baptist.de.bondt@student.ehb.be>
2019-05-23 12:42:32 +0200

    topere code

--- js/cardsPopup.js
+++ js/cardsPopup.js
@@ -1,5 +1,6 @@
 console.log("script linked");
 
+
 $("#buttonOne").on("click", function () {
     $.ajax({
         url: "cards.json",
@@ -15,8 +16,8 @@ $("#buttonOne").on("click", function () {
         console.log(err1);
         console.log(err2);
     });
-    
-    });
+});
+
 
 $("#buttonTwo").on("click", function () {
     $.ajax({
@@ -33,66 +34,68 @@ $("#buttonTwo").on("click", function () {
         console.log(err1);
         console.log(err2);
     });
-    
-    });
-    
-    
-    function printCard(data) {   
-                
-        var color = data.color;
-
-        $("#popupCard").css("background-color", color);
-        
-        var tekst = $('<h1>').text(data.name); // tekst in een p
-        $('#popupCard').append(tekst);
-
-        var leftDiv = $('<div id="leftDiv">');
-        $('#popupCard').append(leftDiv);
-
-        var rightDiv = $('<div id="rightDiv">');
-        $('#popupCard').append(rightDiv);
-
-        var Contact = $('<h3>').text("Contact");
-        $('#leftDiv').append(Contact);
-
-        for (var b in data.contact) {
-
-            var checkbox = $('<input type="checkbox">');
-            checkbox.attr('id', "check" + data.contact[b]);
-
-            var tekstCheck = $('<label>').text(data.contact[b]);
-
-            var br = $('<br>');
+});
 
 
-            $('#leftDiv').append(checkbox);
-            $('#leftDiv').append(tekstCheck);
-            $('#leftDiv').append(br);
-        }
-
-        var Digitaal = $('<h3>').text("Digitaal");
-        $('#rightDiv').append(Digitaal);
-
-        for (var b in data.digitaal) {
-
-            var checkbox = $('<input type="checkbox">');
-            checkbox.attr('id', "check" + data.digitaal[b]);
-
-            var tekstCheck = $('<label>').text(data.digitaal[b]);
-
-            var br = $('<br>');
+var popupCard = document.getElementById("popupCard");
+window.onclick = function (event) {
+    if (event.target == popupCard) {
+        popupCard.style.display = "none";
+    }
+}
 
 
-            $('#rightDiv').append(checkbox);
-            $('#rightDiv').append(tekstCheck);
-            $('#rightDiv').append(br);
-        }
+function printCard(data) {
+    var color = data.color;
 
-      /*  var Description = $('<p>').text(data.description);
-        $('#popupCard').append(Description);*/
-        
-    };
+    var popupContent = $('<div id="popupContent">');
+    $('#popupCard').append(popupContent);
 
+    $("#popupContent").css("background-color", color);
 
+    var tekst = $('<h1>').text(data.name);
+    $(popupContent).append(tekst);
 
+    var leftDiv = $('<div id="leftDiv">');
+    $(popupContent).append(leftDiv);
 
+    var rightDiv = $('<div id="rightDiv">');
+    $(popupContent).append(rightDiv);
+
+    var Contact = $('<h3>').text("Contact");
+    $('#leftDiv').append(Contact);
+
+    for (var b in data.contact) {
+        var checkbox = $('<input type="checkbox">');
+        checkbox.attr('id', "check" + data.contact[b]);
+
+        var tekstCheck = $('<label>').text(data.contact[b]);
+
+        var br = $('<br>');
+
+        $('#leftDiv').append(checkbox);
+        $('#leftDiv').append(tekstCheck);
+        $('#leftDiv').append(br);
+    }
+
+    var Digitaal = $('<h3>').text("Digitaal");
+    $('#rightDiv').append(Digitaal);
+
+    for (var b in data.digitaal) {
+
+        var checkbox = $('<input type="checkbox">');
+        checkbox.attr('id', "check" + data.digitaal[b]);
+
+        var tekstCheck = $('<label>').text(data.digitaal[b]);
+
+        var br = $('<br>');
+
+        $('#rightDiv').append(checkbox);
+        $('#rightDiv').append(tekstCheck);
+        $('#rightDiv').append(br);
+    }
+
+    /*  var Description = $('<p>').text(data.description);
+      $('#popupCard').append(Description);*/
+
+};
